0ad8ebfb8a9f141ede62ff533c4f6eb7ebcde8af
neederhow <decret@outlook.com>
2024-06-15 18:10:33 +0800

    添加文件树窗口的开关按键隐射

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -30,3 +30,5 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- 打开nvim tree
+vim.keymap.set('n', '<leader>tt', '<cmd>NvimTreeToggle<CR>', { desc = '[T]oggle nvim [T]ree' })
